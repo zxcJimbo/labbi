1ac5c9249f5b35173a2605ff3a757e5f5d53c295
zxcJimbo <gunko.kostya1207@gmail.com>
2025-10-30 12:56:07 +0300

    w

---
+++ LAB4/lab4.py
@@ -0,0 +1,16 @@
+from my_module import *
+import datetime
+
+num=int(input("Enter your number: "))
+
+name = str(input("Enter your name:"))
+
+
+dt_now = datetime.datetime.now()
+
+print(dt_now)
+
+print(numberSqrt(num))
+
+print(enterString(name))
+
